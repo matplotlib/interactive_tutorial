5e5ffe19f4ce65deaf679061797aad6b0faeaf43
Thomas A Caswell <tcaswell@gmail.com>
2017-11-06 11:06:31 -0500

    MNT: move weather data around and pull data for ORNL

---
+++ weather/key/04-custom_plotting.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+from pddc_helpers import load_bwi_data, aggregate_by_month
+plt.ion()
+
+bwi = load_bwi_data()
+bwi_monthly = aggregate_by_month(bwi)
+
+fig, ax = plt.subplots()
+
+def plot_aggregated_errorbar(ax, gb, label, picker=None, *,
+                             bands=None,
+                             center_line='mean',
+                             **kwargs):
+    if bands is None:
+        bands = ['min', 'max']
+    kwargs.setdefault('capsize', 3)
+    kwargs.setdefault('markersize', 5)
+    kwargs.setdefault('marker', 'o')
+    eb = ax.errorbar(gb.index, center_line,
+                     yerr='std',
+                     data=gb,
+                     label=label,
+                     picker=picker,
+                     **kwargs)
+    fill = ax.fill_between(gb.index, *bands, alpha=.5,
+                           data=gb, color=eb[0].get_color())
+    ax.legend()
+    ax.figure.canvas.draw_idle()
+    return eb, fill
+
+arts = plot_aggregated_errorbar(ax, bwi_monthly, 'bwi',
+                                bands=['25%', '75%'],
+                                center_line='50%')
